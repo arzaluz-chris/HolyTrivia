ea64016e711c63af39d3238a23d62084730312ee
arzaluz-chris <23008381@es.uveg.edu.mx>
2025-05-12 22:22:00 -0600

    Initial Commit

---
+++ HolyTrivia/HolyTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolyTriviaApp.swift
+//  HolyTrivia
+//
+//  Created by Christian Arzaluz on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HolyTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
